37a858c34eb519f178c28c85b56ae7067119798a
hejiajun530 <hejiajun530@126.com>
2020-07-01 23:40:08 +0800

    init

---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="login">
+    <h1>登录</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background: #344a5f;
+}
+</style>
